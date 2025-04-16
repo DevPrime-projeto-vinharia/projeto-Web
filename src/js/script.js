e2397824f347f77f99b4b00bb139a97f0bd40b47
Jean <jeancarlos12762@gmail.com>
2025-04-16 11:41:37 -0300

    Exercicios 1 e 2

---
+++ src/js/script.js
@@ -0,0 +1,27 @@
+// Exercicio 01
+
+let varivelIndefinida;
+console.log (varivelIndefinida);
+
+let variavelDefinida = "variável definida"
+console.log (variavelDefinida);
+
+// Exercicio 2
+
+// Os operadores !=, == e >= significam respectivamente: Se não igual a, igual a, e maior ou igual a.
+
+let valorExemplo = 10;
+if (valorExemplo != 15);
+    console.log(`valor igual a ${valorExemplo}, exemplo de !=`);
+
+let valorExemplo2 = 10;
+if (valorExemplo2 == 10)
+    console.log(`valor igual a ${valorExemplo2}, exemplo de ==`);
+else 
+    console.log (`valor não é igual a ${valorExemplo2}`);
+
+let valorExemplo3 = 10;
+if (valorExemplo3 >= 10)
+    console.log(`valor igual a ${valorExemplo3}, exemplo de >=`);
+else 
+    console.log (`valor não é igual a ${valorExemplo3}`);
