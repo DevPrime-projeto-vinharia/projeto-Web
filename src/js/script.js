f21e787c0fbcefc0c58dcc8c903d53fa6f4c4902
Jean <jeancarlos12762@gmail.com>
2025-04-16 12:43:50 -0300

    Exercicios 5 e 6

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,5 @@
 // Exercicio 01
+console.log("EXERCICIO 1:")
 
 let varivelIndefinida;
     console.log (varivelIndefinida);
@@ -7,6 +8,7 @@ let variavelDefinida = "variável definida";
     console.log (variavelDefinida);
 
 // Exercicio 2
+console.log("EXERCICIO 2:")
 
 // Os operadores !=, == e >= significam respectivamente: Se não igual a, igual a, e maior ou igual a.
 
@@ -27,9 +29,9 @@ let valorExemplo3 = 10;
         console.log(`valor igual a ${valorExemplo3}, exemplo de >=`);
     else 
         console.log (`valor não é igual a ${valorExemplo3}`);
-
-
-// EX 3
+   
+// Exercicio 3
+console.log("EXERCICIO 3:")
 
 let peso = 200;
 let altura = 1.70;
@@ -52,13 +54,43 @@ switch (true) {
         break;
     }
 
-
-
-
-// EX 4
+// Exercicio 4
+console.log("EXERCICIO 4:")
 
 var valor;
 
 for(valor = 0; valor < 51; valor++){
     console.log(`o valor é: ${valor}`);
 }
+
+// Exercicio 5
+console.log("EXERCICIO 5:")
+
+let usuario = "admin";
+let senha = "1234";
+
+let login = prompt("Insira o login");
+let senhaLogin = prompt("Insira a senha");
+
+if (usuario == login && senha == senhaLogin)
+    console.log("Login realizado com sucesso!");
+else
+    console.log("Usuário ou senha incorretos, tente novamente!");
+
+// Exercicio 6
+console.log("EXERCICIO 6:")
+
+let nota1 = 6;
+let nota2 = 6;
+let nota3 = 6;
+let nota4 = 6;
+let nota5 = 6;
+let nota6 = 6;
+let nota7 = 5;
+
+let media = (nota1+nota2+nota3+nota4+nota5+nota6+nota7)/7
+
+if (media >= 6)
+    console.log(`Parabéns, você foi aprovado :) com media ${media}`)
+else
+    console.log(`Infelizmente você foi Reprovado :( com media ${media}`)
